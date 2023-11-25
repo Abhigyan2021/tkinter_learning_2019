58b2d1179c46501124a8cf3ac923fa71d9a4cdbe
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-25 19:38:06 +0530

    Creating basic windows in python

---
+++ cwh_tkinter1.py
@@ -0,0 +1,17 @@
+
+from tkinter import *
+
+_root = Tk()
+
+# Width x Height
+_root.geometry("644x434")
+
+# width, height
+_root.minsize(400,300)
+
+# width, height
+_root.maxsize(700, 600)
+
+_root.mainloop()
+
+
