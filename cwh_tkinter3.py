d860abe6f9a0cfae5470ac843f4a1d015e583937
Abhigyan2021 <aadeshabhigyan09@gmail.com>
2023-11-26 22:13:18 +0530

    Pack attributes in tkinter

--- cwh_tkinter3.py
+++ cwh_tkinter3.py
@@ -5,7 +5,7 @@ root.title("My GUI with Harry")
 
 
 '''
-Important Label Options:
+Important Label Attribute:
 
 text = adds the text
 bd = background
@@ -21,16 +21,25 @@ relief = border styling(SUNKEN, RAISED, GROOVE, RIDGE)
 
 '''
 
-title_label = Label(text = '''Abdul Rashid Salim Salman Khan is an Indian actor, film producer, and television personality who works predominantly in Hindi films. \nIn a career spanning over three decades, Khan has received numerous awards, including two National Film Awards as a film producer, and a Filmfare Award as an actor.\n He is cited in the media as one of the most commercially successful actors of Indian cinema.\nForbes has included Khan in listings of the highest-paid celebrities in the world, in 2015 and 2018, with him being the highest-ranked Indian in the latter year.\n
-
+title_label = Label(text = '''Abdul Rashid Salim Salman Khan is an Indian actor, film producer, and television personality who works predominantly in Hindi films. In a career spanning over three decades, Khan has received numerous awards, including two National Film Awards as a film producer, and a Filmfare Award as an actor.\n He is cited in the media as one of the most commercially successful actors of Indian cinema.Forbes has included Khan in listings of the highest-paid celebrities in the world, in 2015 and 2018, with him being the highest-ranked Indian in the latter year.\n
 Khan began his acting career with a supporting role in Biwi Ho To Aisi (1988), followed by his breakthrough with a leading role in Sooraj Barjatya's romance Maine Pyar Kiya (1989). \nHe established himself in the 1990s, with several commercially successful films, including Barjatya's family dramas Hum Aapke Hain Koun..! (1994) and Hum Saath-Saath Hain (1999), the action film Karan Arjun (1995) and the comedy Biwi No.1 (1999). 
-
-''',
-bg = "violet", fg = "white", padx= 30, pady = 30, font = "comicsansms 19 bold", borderwidth = 3, relief = SUNKEN
-
+''', bg = "violet", fg = "white", padx= 30, pady = 30, font = "comicsansms 19 bold", borderwidth = 3, relief = SUNKEN
 )
 
+'''
+Important Pack Attributes:
 
-title_label.pack()
+anchor = nw, sw, ne, se
+side = top, bottom, left, right
+fill
+padx
+pady
+
+'''
+
+# title_label.pack()
+
+# title_label.pack(side=BOTTOM, anchor= "sw", fill=X)
+title_label.pack(side=LEFT, anchor = "nw" ,fill=Y)
 
 root.mainloop()
